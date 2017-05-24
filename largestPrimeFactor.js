0e09890719a4d51cf4a8c2928bfd8be6317a3645
Joshua Lee <jdl93@joshuas-air.gateway.ht.net>
2017-05-23 20:54:43 -1000

    trying to make sense of the problem

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,14 +1,22 @@
+//find if a number is prime
+//What is the largest prime factor of the number n?
+
 exports.largestPrimeFactor = function(n){
-  var factors = [];
-  var divider = 2;
+  var factors = []; //array for collecting factors
+  var divider = 2; //If divider goes equally into n, then reassign n to the result of that division
 
   while (n > 1){
-    while((n % divider)=== 0){
+    while((n % divider)=== 0){ //while the number divides equally into the divider
       factors.push(divider);
       n /= divider;
-}
-divider += 1;
+  }
+  divider += 1;
+  //console.log(n);
+  //console.log(factors);
+  //console.log(divider);
 
   }
-  return factors[factors.length - 1];
+  console.log(factors[factors.length - 1]);
+  return factors[factors.length - 1]; //returning the last factor from the array
+
 };
